6da90484de1299a496d87f500586259c656600ac
nikhilChandra1 <45300482+nikhilChandra1@users.noreply.github.com>
2020-07-03 19:00:20 -0700

    Add files via upload

---
+++ Leland Speech and Debate App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Leland Speech and Debate App
+//
+//  Created by Nikhil Chandra on 7/2/20.
+//  Copyright © 2020 Nikhil Chandra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Leland Speech and Debate AppTests/Leland_Speech_and_Debate_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Leland_Speech_and_Debate_AppTests.swift
+//  Leland Speech and Debate AppTests
+//
+//  Created by Nikhil Chandra on 7/2/20.
+//  Copyright © 2020 Nikhil Chandra. All rights reserved.
+//
+
+import XCTest
+@testable import Leland_Speech_and_Debate_App
+
+class Leland_Speech_and_Debate_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
